740b29a1aac01853d9f7add298660f7ad2d4dec0
MinJung <minjungzung@gmail.com>
2023-09-20 23:01:03 +0900

    [삼성SW역량테스트] 0919 스터디

---
+++ src/삼성SW역량테스트/13460_구슬탈출2.py
@@ -0,0 +1,63 @@
+'''
+bfs에서 방문 체크는 '큐에 삽입하는 순간' 해야함
+'''
+n, m = map(int, input().split())
+board = [list(input()) for _ in range(n)]
+
+rx, ry, bx, by = 0, 0, 0, 0
+
+for i in range(n):
+    for j in range(m):
+        if board[i][j] == 'R':
+            rx, ry = i, j
+        elif board[i][j] == 'B':
+            bx, by = i, j
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+def move(x, y, d):
+    cnt = 0
+    while board[x][y] != 'O' and board[x+dx[d]][y+dy[d]] != '#':
+        x += dx[d]
+        y += dy[d]
+        cnt += 1
+    return x, y, cnt
+
+
+def bfs(rx, ry, bx, by):
+    q = [(rx, ry, bx, by, 1)]
+    visited = [(rx, ry, bx, by)]    # 파란 구슬의 위치도 방문 체크에 포함해야함
+    while q:
+        rx, ry, bx, by, cnt = q[0]
+        q.remove(q[0])
+
+        if cnt > 10:
+            break
+
+        for i in range(4):
+            nrx, nry, rCnt = move(rx, ry, i)
+            nbx, nby, bCnt = move(bx, by, i)
+
+            if board[nbx][nby] == 'O':
+                continue
+
+            if board[nrx][nry] == 'O':
+                return cnt
+
+            if (nrx, nry) == (nbx, nby):
+                if rCnt > bCnt:
+                    nrx -= dx[i]
+                    nry -= dy[i]
+                else:
+                    nbx -= dx[i]
+                    nby -= dy[i]
+
+            if (nrx, nry, nbx, nby) not in visited:
+                visited.append((nrx, nry, nbx, nby))
+                q.append((nrx, nry, nbx, nby, cnt + 1))
+    return -1
+
+
+print(bfs(rx, ry, bx, by))
