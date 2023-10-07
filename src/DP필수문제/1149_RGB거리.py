1bfea069f122427d19e797d9dac8a2ca3474e5db
MinJung <minjungzung@gmail.com>
2023-10-07 21:46:25 +0900

    [DP필수문제] 주석 추가

--- src/DP필수문제/1149_RGB거리.py
+++ src/DP필수문제/1149_RGB거리.py
@@ -6,11 +6,12 @@
 '''
 n = int(input())
 score = [list(map(int, input().split())) for _ in range(n)]
-dp = [[0] * 3 for _ in range(n)]
+dp = [[0] * 3 for _ in range(n)] # 각 색상에 대한 누적 최소값
 dp[0] = [i for i in score[0]]
 
 for i in range(1, n):
     for j in range(3):
+        # 현재 색상과 같지 않은 dp[i-1]의 색 2개의 min
         dp[i][j] = min(dp[i - 1][(j - 1) % 3], dp[i - 1][(j - 2) % 3]) + score[i][j]
 
 print(min(dp[n-1]))
